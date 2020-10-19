48d6d44b2ad7d9c4cd968576c280dfb641466113
Forklein <59373930+Forklein@users.noreply.github.com>
2020-10-19 10:57:13 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,26 @@
 nome = input('Ciao!, inserisci il tuo nome: ')
-print ('Ciao ' + nome + '! Questo è il bot calculator di Peppe')
+print('Ciao ' + nome + '! Questo è il bot calculator di Peppe')
 numero = float(input('Inserisci il primo numero: '))
-op = input('Inserisci operatore: ')
 numeroo = float(input('Inserisci il secondo numero: '))
-if op == '+':
- print(numero+numeroo)
-elif op == '-':
- print(numero-numeroo)
-elif op == '*':
- print(numero*numeroo)
-elif op == '/':
- print(numero/numeroo)
-else:
- print('Hai inserito un operatore non corretto')
+gang = True
+while gang == True:
+ op = input('Inserisci operatore: ')
+ if op == '+':
+     print('Il risultato è: ', end ='')
+     print(numero+numeroo)
+     break
+ elif op == '-':
+     print('Il risultato è: ', end ='')
+     print(numero-numeroo)
+     break
+ elif op == '*':
+     print('Il risultato è: ', end ='')
+     print(numero*numeroo)
+     break
+ elif op == '/':
+     print('Il risultato è: ', end ='')
+     print(numero/numeroo)
+     break
+ else:
+     print('Hai inserito un operatore non corretto, riprova!')
+     continue
